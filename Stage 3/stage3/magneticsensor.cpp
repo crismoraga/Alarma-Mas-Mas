ed03e619302067797e47d0d595c34acf94270140
Clmujica <clmujica@alumnos.inf.utfsm.cl>
2023-06-15 00:39:31 -0400

    Sensores update

--- Stage 3/stage3/magneticsensor.cpp
+++ Stage 3/stage3/magneticsensor.cpp
@@ -1,13 +1,13 @@
 #include "magneticsensor.h"
 
-MagneticSensor::MagneticSensor(int z): ??? {
+MagneticSensor::MagneticSensor(int z): Sensor(z) {
     view = new MagneticSensorView();
 }
 void MagneticSensor::setSensorOpen() {
-    ??
+    this->setClose(false);
 }
 void MagneticSensor::setSensorClose() {
-   ??
+   this->setClose(true);
 }
 MagneticSensorView * MagneticSensor::getView() {
     return view;
--- Stage 3/stage3/sensor.cpp
+++ Stage 3/stage3/sensor.cpp
@@ -2,15 +2,16 @@
 #include <iostream>
 using namespace std;
 Sensor::Sensor(int z, bool close){
-   ???
+    zone = z;
+    close = close;
 }
 bool Sensor::isClose() const {
-    ??
+    return close;
 }
 int Sensor::getZone() const {
-    ??
+    return zone;
 }
 void Sensor::setClose(bool isClose) {
-    ??
+    close = isClose;
     cout << "Sensor is "<< (close?"close.":"open.") << endl;
 }
