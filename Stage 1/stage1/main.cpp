eafd1afb3da185d15e77cb096cc7a0476a3ffe53
Clmujica <clmujica@alumnos.inf.utfsm.cl>
2023-06-17 21:28:08 -0400

    Listo

--- Stage 1/stage1/main.cpp
+++ Stage 1/stage1/main.cpp
@@ -8,14 +8,14 @@ int main(){
     ifstream fin;
     int nDoors, junk;
     vector<Door *> doors;
-    /*
+    
     if (argc != 2) {
         cout << "Usage: "<<argv[0]<<" <configuration_file>" << endl;
         return -1;
     }
     fin.open(argv[1]);
-*/
-    fin.open("C:/Users/Moragax3/Desktop/Tarea3 Poo/stage1/config.txt");
+
+    //fin.open("C:/Users/Moragax3/Desktop/Tarea3 Poo/stage1/config.txt");
     if (fin.fail()){
         cout << "Could not read file" << endl;
         return -2;
